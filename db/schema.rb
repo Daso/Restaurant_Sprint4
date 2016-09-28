4b6c562656dd3ea6c609369b7c25c8ce7baff1fe
daso <daalsuol@gmail.com>
2016-09-28 21:41:03 +0000

    spring 4

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160924012029) do
+ActiveRecord::Schema.define(version: 20160928212043) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
@@ -24,8 +24,12 @@ ActiveRecord::Schema.define(version: 20160924012029) do
     t.string   "name"
     t.text     "description"
     t.integer  "category_id"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at",           null: false
+    t.datetime "updated_at",           null: false
+    t.string   "picture_file_name"
+    t.string   "picture_content_type"
+    t.integer  "picture_file_size"
+    t.datetime "picture_updated_at"
   end
 
   add_index "products", ["category_id"], name: "index_products_on_category_id"
